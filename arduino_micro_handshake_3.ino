0cbc76c67b8436c4202cf1f9e70eb0911bd8119d
AndruOn <48825640+AndruOn@users.noreply.github.com>
2019-06-15 11:30:17 +0200

    Add files via upload

---
+++ arduino_micro_handshake_3.ino
@@ -0,0 +1,22 @@
+int V= 0;
+
+// the setup routine runs once when you press reset:
+void setup() {
+        // on démarre la liaison
+        // en la réglant à une vitesse de 115200 bits par seconde.
+        Serial.begin(115200);
+}
+void loop() {
+  // main code here, that runs repeatedly:
+
+  // Setup pins for input
+  //handshake test only execute when receive command
+    if(Serial.read()=='K') {
+          for (int i = 0; i < 3; i++) { 
+                      int V = analogRead(i);
+                      Serial.print(V);
+                      Serial.print(" ");
+                      
+                  }
+        }
+}
